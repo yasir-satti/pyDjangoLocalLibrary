9e0fb967e1db3e1d89361abed9dc214ee80973ab
yasir satti <yassir.satti@hotmail.com>
2022-12-25 20:18:53 +0000

    manage admin site

--- catalog/models.py
+++ catalog/models.py
@@ -45,6 +45,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 
 import uuid # Required for unique book instances
@@ -96,4 +102,5 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
 
